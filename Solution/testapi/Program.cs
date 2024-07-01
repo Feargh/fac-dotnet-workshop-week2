e8c764bb6555e183a0386c17609e69be84c9dfc4
ShaughnAnderson94 <113926900+ShaughnAnderson94@users.noreply.github.com>
2024-07-01 12:55:34 +0100

    Update Program.cs

--- Solution/testapi/Program.cs
+++ Solution/testapi/Program.cs
@@ -1,6 +1,5 @@
 using Microsoft.EntityFrameworkCore;
 using testapi.Data;
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -9,10 +8,12 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<GamesDbContext>(options =>
+builder.Services.AddDbContext<iGamesDbContext, GamesDbContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
+using(var scope = app.Services.CreateScope())
+using(var db = scope.ServiceProvider.GetService<GamesDbContext>()!)
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
